25bb027941a144485a0322d6114c8872a5741e7a
pavan <mendepavan2004@gmail.com>
2025-06-23 10:25:31 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -46,9 +46,13 @@ app.use("/api/status",(req,res)=>{
 app.use("/api/auth",userRouter)
 app.use("/api/messages",messageRouter)
 await connectDB()
+if(process.env.NODE_ENV !=="production"){
 const PORT=process.env.PORT || 5000
-
 server.listen(PORT,()=>{
     console.log("server is running on port",PORT)
 })
+}
+
+// exporting server for vercel
+export default server
 
